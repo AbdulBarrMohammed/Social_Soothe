0346d2eb51e24176ad1a1affd25275043108a94c
Barr Mohammed <barrmohammed.tpa@gmail.com>
2024-12-11 22:13:03 -0500

    testing

--- frontend/src/data/moodData.js
+++ frontend/src/data/moodData.js
@@ -5,7 +5,7 @@ export const lineChartData = {
     datasets: [
         {
             label: "Mood",
-            data: [3000, 5000, 4500, 6000, 8000, 7000, 9000],
+            data: [3000, 5000, 4500, 6000, 8000, 7000, 8000],
             borderColor: "rgb(75, 192, 192)",
             fill: false,  // Optional: adds a line without filling the area beneath it
             tension: 0.1   // Optional: smoothens the line
